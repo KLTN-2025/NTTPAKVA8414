a32ef3ebd413f470875e26867aedcb0ddcf1883b
Thomashoang99 <hoang1999tom@gmail.com>
2025-11-09 23:44:15 +0700

    Thêm một số tính năng quản lý đơn hàng cho client

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -9,4 +9,28 @@ router.post('/',
   orderController.placeOrder
 );
 
+/**
+ * Get all orders made by a specific user;
+ * Requires auth.
+ * */ 
+router.get('/', requireAuth(),
+  orderController.getOrders
+)
+
+/**
+ * Get the product list in a specific order;
+ * Requires auth.
+ */
+router.get('/:id', requireAuth(),
+  orderController.getOrderDetails
+)
+
+/**
+ * Cancel an order;
+ * Requires auth.
+ */
+router.put('/:id/cancel', requireAuth(),
+  orderController.cancelOrder
+)
+
 module.exports = router
